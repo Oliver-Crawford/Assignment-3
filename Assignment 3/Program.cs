3a4a3e0a478b1a3f6ec528e280c8dd0e0797ec90
Oliver Crawford <oicrawford@gmail.com>
2024-02-01 23:28:26 -0600

    Finished adding Creating a student, Editing a student, and viewing all students.

--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -27,14 +27,43 @@ namespace Assignment_3
         }
         static void Main(string[] args)
         {
-            DisplayMenu();
-            while (true)
+            string studentFolderName = "students";
+            bool running = true;
+            string chooseText = "Please Choose: ";
+
+            while (running)
             {
-                
-                switch
-                break;
+                DisplayMenu();
+                Console.Write(chooseText);
+                switch (SanitizeIntInput())
+                {
+                    case 1:
+                        CreateStudent(studentFolderName);
+                        break;
+                    case 2:
+                        Console.Write("Please input ID: ");
+                        EditStudent(SanitizeIntInput(), studentFolderName);
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+                        ViewAllStudents(studentFolderName);
+                        break;
+                    case 0:
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid option, try again.");
+                        break;
+                }
             }
-            Console.ReadLine();
         }
         static void DisplayMenu()
         {
@@ -48,7 +77,6 @@ namespace Assignment_3
                 }
             }
         }
-
         static int SanitizeIntInput()
         {
             bool finished = false;
@@ -63,6 +91,137 @@ namespace Assignment_3
                     Console.WriteLine("Invalid input, please enter a whole number.");
                 }
             }
+            return intInput;
+        }
+        static string SanitizeStringInput(string failString = "")
+        {
+            bool finished = false;
+            string input = "";
+            while (!finished)
+            {
+                input = Console.ReadLine();
+                if(input == "")
+                {
+                    Console.WriteLine("Invalid input, please input a string.");
+                    Console.Write(failString);
+                }
+                else
+                {
+                    finished = true;
+                    Console.Write(failString);
+                }
+            }
+            return input;
+        }
+        static float SanitizeFloatInput(string failString = "")
+        {
+            bool finished = false;
+            string input = "";
+            float floatInput = 0f;
+            while (!finished)
+            {
+                input = Console.ReadLine();
+                finished = float.TryParse(input, out floatInput);
+                if (!finished)
+                {
+                    Console.WriteLine("Invalid input, please input float.");
+                    Console.Write(failString);
+                }
+            }
+            return floatInput;
+        }
+        static void CreateStudent(string studentFolderName)
+        {
+            int id = 0;
+            string name;
+            int age;
+            string address;
+            float gpa;
+            Console.Write("Input Student Name: ");
+            name = SanitizeStringInput();
+            Console.WriteLine("");
+            Console.Write("Input Student Age: ");
+            age = SanitizeIntInput();
+            Console.WriteLine("");
+            Console.Write("Input Student Address: ");
+            address = SanitizeStringInput();
+            Console.WriteLine("");
+            Console.Write("Input Student GPA: ");
+            gpa = SanitizeFloatInput();
+            Console.WriteLine("");
+
+            while (true)
+            {
+                if (File.Exists($"{studentFolderName}\\{id}"))
+                {
+                    id++;
+                }
+                else
+                {
+                    using(StreamWriter file = new StreamWriter($"{studentFolderName}\\{id}"))
+                    {
+                        file.WriteLine(id); file.WriteLine(name); file.WriteLine(age); file.WriteLine(address); file.WriteLine(gpa);
+                    }
+                    break;
+                }
+            }
+
+        }
+        static void ViewAllStudents(string studentFolderName)
+        {
+            Console.WriteLine("ID |  Name |  Age |  Address |  GPA");
+            foreach(string file in Directory.GetFiles(studentFolderName))
+            {
+                using (StreamReader sr = new StreamReader(file))
+                { 
+                    Console.WriteLine($"  {sr.ReadLine()} |  {sr.ReadLine()} |  {sr.ReadLine()} |  {sr.ReadLine()} |  {sr.ReadLine()}");
+                }
+            }
+        }
+        static void EditStudent(int id, string studentFolderName)
+        {
+            if (!File.Exists($"{studentFolderName}\\{id}"))
+            {
+                Console.WriteLine("Id does not exist!");
+                return;
+            }
+            string oldName = "";
+            int oldAge = 0;
+            string oldAddress = "";
+            float oldGpa = 0;
+            string name;
+            int age;
+            string address;
+            float gpa;
+            using (StreamReader sr = new StreamReader($"{studentFolderName}\\{id}"))
+            {
+                sr.ReadLine();
+                oldName = sr.ReadLine();
+                int.TryParse(sr.ReadLine(), out oldAge);
+                oldAddress = sr.ReadLine();
+                float.TryParse(sr.ReadLine(), out oldGpa);
+            }
+
+            Console.WriteLine($"Old Name was {oldName}");
+            Console.Write("Enter new name: ");
+            name = SanitizeStringInput();
+            Console.Write("Enter new Age: ");
+            age = SanitizeIntInput();
+            Console.Write("Enter new Address: ");
+            address = SanitizeStringInput();
+            Console.Write("Enter new GPA: ");
+            gpa = SanitizeFloatInput();
+
+            File.Delete(($"{studentFolderName}\\{id}"));
+            using(StreamWriter sw = new StreamWriter($"{studentFolderName}\\{id}"))
+            {
+                sw.WriteLine(id);
+                sw.WriteLine(name);
+                sw.WriteLine(age);
+                sw.WriteLine(address);
+                sw.WriteLine(gpa);
+            }
+
         }
     }
     
